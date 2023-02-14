c10be4c1fda880c52116406cf66c7924e4082489
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-02-13 22:17:09 -0300

    Create EX18.java

---
+++ AED III/Capítulo 1/EX18.java
@@ -0,0 +1,12 @@
+package edIII;  
+public class Atividade_2_18 {  
+ public static void main(String[] args) {  
+ String[] equipes = {  
+ "Santos","Corinthians",  
+ "São Paulo","Portuguesa",  
+ "Palmeiras","São Caetano" 
+ };  
+ System.out.println("Seu time favorito hoje = " +  
+ equipes[3]);  
+ }  
+ }  
