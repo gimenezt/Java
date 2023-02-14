d7dc93c5a9d8da5083cc42d1e91212c415b91f04
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-02-13 22:14:37 -0300

    Create EX09.java

---
+++ AED III/Capítulo 1/EX09.java
@@ -0,0 +1,13 @@
+package edIII;  
+import java.util.Arrays;  
+public class Atividade_2_9 {  
+public static void main(String[] args) {  
+  
+ int[] x = new int[] { 3, 1, 6, 2, 9, 5, 0} ;  
+  
+ Arrays.sort(x);  
+  
+ for ( int elem : x )  
+ System.out.print(elem + " ");  
+ }  
+} 
