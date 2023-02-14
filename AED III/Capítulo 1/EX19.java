87d783f1971e9e44234fc297f17dfb41b1e11315
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-02-13 22:17:21 -0300

    Create EX19.java

---
+++ AED III/Capítulo 1/EX19.java
@@ -0,0 +1,11 @@
+package edIII;  
+public class Atividade_2_19 {  
+public static void main(String[] args) {  
+ String a = new String("USCS");  
+ String b = new String("Computacao");  
+ a = b;  
+ b = a;  
+ System.out.println(a);  
+ System.out.println(b);  
+ }  
+} 
