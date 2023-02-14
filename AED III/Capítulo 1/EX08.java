281f4df46f11137eb988b6d0e4b45636f5a62712
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-02-13 22:14:19 -0300

    Create EX08.java

---
+++ AED III/Capítulo 1/EX08.java
@@ -0,0 +1,13 @@
+package edIII;  
+public class Atividade_2_8 {  
+public static void main(String[] args) {  
+  
+ int[] x = new int[4];  
+  
+ if (x == null)  
+ System.out.println("x tem valor nulo...");  
+  
+ else  
+ System.out.println("x NAO tem valor nulo...");  
+ }  
+} 
