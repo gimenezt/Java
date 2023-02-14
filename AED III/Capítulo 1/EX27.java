a6fee703287c654cde45b52015c0cf48ca12453e
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-02-13 22:20:18 -0300

    Create EX27.java

---
+++ AED III/Capítulo 1/EX27.java
@@ -0,0 +1,11 @@
+package edIII;  
+public class Atividade_2_27 {  
+public static void main(String[] args) {  
+  
+ String S1 = args[0];  
+ String S2 = null;  
+ S2 = args[1]+args[0];  
+ String S3 = new String(S2.toUpperCase()) ;  
+ System.out.println(S1+S2+S3);  
+ }  
+} 
