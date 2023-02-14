56e35b5d075ff346d2fccc6cb4392ea51a8eec43
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-02-13 22:20:39 -0300

    Create EX28.java

---
+++ AED III/Capítulo 1/EX28.java
@@ -0,0 +1,16 @@
+package edIII;  
+public class Atividade_2_28 {  
+  
+public static void main(String[] args) {  
+  
+ String S1 = args[0];  
+ String S2 = null;  
+ S2 = args[1]+args[0];  
+  
+ int x = Integer.parseInt(S2);  
+  
+ int y = Integer.parseInt(S1);  
+  
+ System.out.println(x+y);  
+ }  
+}  
