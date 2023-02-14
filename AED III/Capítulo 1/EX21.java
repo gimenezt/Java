dc7d74c7923562b230250f14281640a783aa4764
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-02-13 22:17:46 -0300

    Create EX21.java

---
+++ AED III/Capítulo 1/EX21.java
@@ -0,0 +1,9 @@
+package edIII;  
+public class Atividade_2_20 {  
+ public static void main(String[] args) {  
+ String a = new String("Hello");  
+ String b = new String(a+"USCS");  
+ String c = new String("a".concat(b));  
+ System.out.println(c);  
+ }  
+} 
