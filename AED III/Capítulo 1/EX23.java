f62861f9ad202323021f82945677de5cb52f9e8e
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-02-13 22:19:19 -0300

    Create EX23.java

---
+++ AED III/Capítulo 1/EX23.java
@@ -0,0 +1,10 @@
+package edIII;  
+public class Atividade_2_23 {  
+public static void main(String[] args) {  
+ String S1 = new String("abcd");  
+ String S2 = null;  
+ S2 = S1;  
+ String S3 = new String(S2.toUpperCase());  
+ System.out.println(S1+S2+S3);  
+ }  
+}  
